87e6e3bffbda3a5c1f0f927eeaa45d3d5e2cdf8f
ri31 <riadibadulla@gmail.com>
2019-03-04 15:18:16 +0000

    New class added

---
+++ src/main/java/DataBase.java
@@ -0,0 +1,43 @@
+
+import java.sql.*;
+
+public class Database {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        String url = "jdbc:mysql://dag8.host.cs.st-andrews.ac.uk/";
+        String db = "dag8_RickDB";
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String user = "ri31";
+        String pass = "33.1Z4HLNfnbuy";
+        
+        try {
+            Class.forName(driver).newInstance();
+            con = DriverManager.getConnection(url + db, user, pass);
+            
+            try {
+                Statement st = con.createStatement();
+                ResultSet rs = st.executeQuery("SELECT * FROM test");
+                while (rs.next()) {
+                    String name = rs.getString("name");
+                    System.out.println(name);
+                }
+
+                //ADD User
+                //st.executeUpdate("INSERT INTO dag8_RickDB.test (name) Values ('Praneeth'), ('John'); ");
+
+
+                //DELETE
+                //st.executeUpdate("DELETE FROM dag8_RickDB.test WHERE name = 'John'");
+
+                //UPDATE
+                //st.executeUpdate("UPDATE dag8_RickDB.test SET name = 'Edwin' WHERE name = 'Edvin'");
+                con.close();
+            } catch (SQLException se) {
+                 System.out.println("SQL ERR: " + se); //
+            }
+        } catch (Exception e) {
+            System.out.println("ERR: " + e);
+        }
+    }
+}
