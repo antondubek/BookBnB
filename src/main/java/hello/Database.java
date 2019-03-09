f1c47f8025ecc9627e6849738cfab5801309398c
dag8 <dag8@pc2-028-l.cs.st-andrews.ac.uk>
2019-03-09 19:03:20 +0000

    changed URL

--- src/main/java/hello/Database.java
+++ src/main/java/hello/Database.java
@@ -10,8 +10,8 @@ import java.util.ArrayList;
 public class Database {
 
     private static Connection con = null;
-    private static String url = "jdbc:mysql://dag8.host.cs.st-andrews.ac.uk/";
-    //private static String url = "jdbc:mysql://localhost:3307/";
+    //private static String url = "jdbc:mysql://dag8.host.cs.st-andrews.ac.uk/";
+    private static String url = "jdbc:mysql://localhost:3307/";
     private static String db = "dag8_RickDB";
     private static String driver = "com.mysql.cj.jdbc.Driver";
     private static String user = "ri31";
