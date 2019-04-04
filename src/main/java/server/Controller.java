80889eceaac8e5f6fa56f53f3feed2919c283b84
riadibadulla <ri31@pc5-036-l.cs.st-andrews.ac.uk>
2019-04-04 13:00:31 +0100

    Javadocs for follow methods are added to Controller

--- src/main/java/server/Controller.java
+++ src/main/java/server/Controller.java
@@ -159,8 +159,8 @@ public class Controller {
 
     /**
      * Update whether or not a book is available for lending. If update successful, send STATUS OK, otherwise send 404
-     * @param jsonString JSON passed in the request body
-     * @return
+     * @param jsonString JSON passed in the request body, contains "email", "available", "ISBN", "copyID"
+     * @return HttpStatus Ok if updated successfully
      */
     @RequestMapping(method= RequestMethod.POST, value = "/profile/books/availability")
     public ResponseEntity<String> updateBookAvailability(@RequestBody String jsonString) {
@@ -188,6 +188,11 @@ public class Controller {
         return (updatedAvailability) ? new ResponseEntity<String>(HttpStatus.OK) : new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * RequestMapping to follow the user
+     * @param jsonString contains "email" and "friendEmail" which it needs to follow
+     * @return (HttpStatus.OK) if followed successfully
+     */
     @RequestMapping(method= RequestMethod.POST, value = "/follow")
     public ResponseEntity<String> follow(@RequestBody String jsonString) {
         JSONObject data = new JSONObject(jsonString);
@@ -203,6 +208,11 @@ public class Controller {
         return (followSuccessful) ? new ResponseEntity<String>(HttpStatus.OK) : new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * RequestMapping to unfollow the user
+     * @param jsonString contains "email" and "friendEmail" which it needs to unfollow
+     * @return (HttpStatus.OK) if unfollowed successfully
+     */
     @RequestMapping(method= RequestMethod.POST, value = "/follow/delete")
     public ResponseEntity<String> deleteFollow(@RequestBody String jsonString) {
         JSONObject data = new JSONObject(jsonString);
@@ -215,6 +225,11 @@ public class Controller {
         return (followSuccesfull) ? new ResponseEntity<String>(HttpStatus.OK) : new ResponseEntity<String>(HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * RequestMapping to check if the user is followed by another user
+     * @param jsonString contains "email" and "friendEmail" which it check if is followed or not
+     * @return JSon object string with field userIsFollowed, true if the user follows friendEmail
+     */
     @RequestMapping(method= RequestMethod.POST, value = "/follow/true")
     public Map<String, Boolean> getIsFollowed(@RequestBody String jsonString) {
         JSONObject data = new JSONObject(jsonString);
