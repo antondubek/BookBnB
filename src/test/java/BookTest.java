ed47e44fac5ce00c162abeaab6a69aa7a16e83c3
dag8 <dag8@pc2-097-l.cs.st-andrews.ac.uk>
2019-04-21 18:33:02 +0100

    book class tests complete

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,51 @@
+import org.json.JSONObject;
+import org.junit.Test;
+import org.junit.Before;
+import server.Book;
+
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+
+public class BookTest {
+    public Book testBook;
+    public String ISBN = "1112223334445";
+    private String title = "Test";
+    private String author = "Testy McTest";
+    private String edition = "1st";
+    private Boolean available;
+    private String copyID = "9998887776665";
+    private Boolean isLoaned = true;
+    private String loanLength = "7";
+
+
+    @Before
+    public void Setup(){
+        testBook = new Book(ISBN, title, author);
+    }
+
+    @Test
+    public void testConstruction(){
+        assertEquals(testBook.getISBN(), ISBN);
+        assertEquals(testBook.getTitle(), title);
+        assertEquals(testBook.getAuthor(), author);
+        assertEquals(testBook.getIsLoaned(), false);
+    }
+
+    @Test
+    public void testSetters() {
+        testBook.setEdition(edition);
+        testBook.setAvailable("1");
+        testBook.setCopyID(copyID);
+        testBook.setIsLoaned(isLoaned);
+        testBook.setLoanLength(loanLength);
+
+        assertEquals(testBook.getEdition(), edition);
+        assertEquals(testBook.getAvailable(), true);
+        assertEquals(testBook.getCopyID(), copyID);
+        assertEquals(testBook.getIsLoaned(), isLoaned);
+        assertEquals(testBook.getLoanLength(), loanLength);
+    }
+
+
+}
